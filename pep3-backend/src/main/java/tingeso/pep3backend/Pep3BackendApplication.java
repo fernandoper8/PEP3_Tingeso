3d7a7c50a81d5db6b485956e5a01ed034e8225f3
Fernando <fernando.perez.c@usach.cl>
2023-07-11 22:04:08 -0400

    Inicializacion proyecto backend

---
+++ pep3-backend/src/main/java/tingeso/pep3backend/Pep3BackendApplication.java
@@ -0,0 +1,13 @@
+package tingeso.pep3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep3BackendApplication.class, args);
+	}
+
+}
---
+++ pep3-backend/src/test/java/tingeso/pep3backend/Pep3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.pep3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
