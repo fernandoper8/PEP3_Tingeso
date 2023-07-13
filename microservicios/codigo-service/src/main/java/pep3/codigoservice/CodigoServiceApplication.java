dc8f22675bbdfcd78c2045a11ac997d0d57d9463
Fernando <fernando.perez.c@usach.cl>
2023-07-12 20:52:50 -0400

    Avances microservicio

---
+++ microservicios/codigo-service/src/main/java/pep3/codigoservice/CodigoServiceApplication.java
@@ -0,0 +1,13 @@
+package pep3.codigoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoServiceApplication.class, args);
+	}
+
+}
---
+++ microservicios/prueba-service/src/main/java/pep3/pruebaservice/PruebaServiceApplication.java
@@ -0,0 +1,13 @@
+package pep3.pruebaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaServiceApplication.class, args);
+	}
+
+}
