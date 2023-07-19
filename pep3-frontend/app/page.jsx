6485ddcdf8b82fa096b25bfcaaeceac17ee61a29
Fernando <fernando.perez.c@usach.cl>
2023-07-19 00:01:09 -0400

    Reinicio vista home. Nuevas imagenes

--- pep3-frontend/app/page.jsx
+++ pep3-frontend/app/page.jsx
@@ -5,33 +5,11 @@ import style from './page.module.css';
 export default function index() {
     return (
         <main className={style.principal}>
-            <div className={style.contenedorPrincipal}>
+            <h1>
+                <span>cyber.py</span>
+            </h1>
 
-                <h1>
-                    <span>cyber.py</span>
-                </h1>
-                
-                
-                <div className={style.contenedorCentral}>
-
-                    <div className={style.imagen}>
-                         <img src="/img/V-landing.webp"/> 
-                    </div>
-
-                    <div className={style.contenedorBotones}>
-                        <button>Crea una evaluación</button>
-                        <button>Ver evaluaciones</button>
-                    </div>
-                    <div className={style.contenedorAbout}>
-                        <p>¿Qué es cyber.py?</p>
-                    </div>
-                </div>
-
-                <footer className={style.footer}>
-                </footer>
-
-            </div>
-
+            <footer className={style.footer} />
         </main>
     )
 }
